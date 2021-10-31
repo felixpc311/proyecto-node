0c6d3c08a9efa42cfbe18c8c1bceb049e3cf997c
felixpc311 <felixpuello311@gmail.com>
2021-10-31 16:57:26 -0500

    HOTFIX: remove comments in simuladorController.js file

--- controllers/simuladorController.js
+++ controllers/simuladorController.js
@@ -6,60 +6,60 @@ module.exports = {
     index:function(req, res){
         res.render('simulador/index', { title: 'Simulator' });
     },
-    // inicioSesion:function(req,res){
-    //     simuladorModel.inicioSesion(con, function(err,datos){
-    //         if (!err) {
-    //             //console.log(datos);
-    //             res.render('simulador/inicioSesion',{ tittle: 'Iniciar Sesion' });
-    //         } else {
-    //             console.log("Error de: "+err);
-    //         }
-    //     });
-    // },
-    // registro:function(req,res){
-    //     simuladorModel.registro(con, function(err,datos){
+    inicioSesion:function(req,res){
+        simuladorModel.inicioSesion(con, function(err,datos){
+            if (!err) {
+                //console.log(datos);
+                res.render('simulador/inicioSesion',{ tittle: 'Iniciar Sesion' });
+            } else {
+                console.log("Error de: "+err);
+            }
+        });
+    },
+    registro:function(req,res){
+        simuladorModel.registro(con, function(err,datos){
             
 
-    //         if (!err) {
-    //             //console.log(datos);
-    //             res.render('simulador/registro',{ tittle: 'Registro' });
-    //         } else {
-    //             console.log("Error de: "+err);
-    //         }
-    //     });
-    //     //res.send('recibido');
-    // },
-    // registroBD:async function(req,res){
-    //     const { nombres_usuario, apellidos_usuario, numero_celular, 
-    //         email_usuario, contrasena_usuario } = req.body;
-    //     const newRegistro = {
-    //         nombres_usuario, apellidos_usuario, numero_celular, 
-    //         email_usuario, contrasena_usuario
-    //     };
-    //     await simuladorModel.registroBD(con, newRegistro, function(err, datos){
-    //         if (!err) {
-    //             alert("Ha sido registrado satisfactoriamente");
+            if (!err) {
+                //console.log(datos);
+                res.render('simulador/registro',{ tittle: 'Registro' });
+            } else {
+                console.log("Error de: "+err);
+            }
+        });
+        //res.send('recibido');
+    },
+    registroBD:async function(req,res){
+        const { nombres_usuario, apellidos_usuario, numero_celular, 
+            email_usuario, contrasena_usuario } = req.body;
+        const newRegistro = {
+            nombres_usuario, apellidos_usuario, numero_celular, 
+            email_usuario, contrasena_usuario
+        };
+        await simuladorModel.registroBD(con, newRegistro, function(err, datos){
+            if (!err) {
+                alert("Ha sido registrado satisfactoriamente");
         
-    //             res.render('simulador/inicioSesion',{ tittle: 'Iniciar Sesion' });
-    //         } else {
-    //             console.log("Error de: "+err);
-    //         }
-    //     });
+                res.render('simulador/inicioSesion',{ tittle: 'Iniciar Sesion' });
+            } else {
+                console.log("Error de: "+err);
+            }
+        });
         
         
 
-    // },
-    // simulador:function(req,res){
-    //     simuladorModel.simulador(con, function(err,datos){
+    },
+    simulador:function(req,res){
+        simuladorModel.simulador(con, function(err,datos){
             
 
-    //         if (!err) {
-    //             console.log(datos);
-    //             res.render('simulador/simulador',{ tittle: 'Simulador' });
-    //         } else {
-    //             console.log("Error de: "+err);
-    //         }
-    //     });
-    //     //res.send('recibido');
-    // }
+            if (!err) {
+                console.log(datos);
+                res.render('simulador/simulador',{ tittle: 'Simulador' });
+            } else {
+                console.log("Error de: "+err);
+            }
+        });
+        //res.send('recibido');
+    }
 }
